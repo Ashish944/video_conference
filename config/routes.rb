6050ca630eff4761d607c8aa6a7065f946310cb4
Ashish <aashishpande944@gmail.com>
2020-05-24 17:03:47 +0530

    add routes for company

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'companies#new'
+
+  devise_for :companies, :skip => [:registration], controllers: { confirmations: 'confirmation' }
+  as :company do
+    get 'companies/new' => 'companies#new', :as => 'new_company_registration'
+    post 'companies' => 'companies#create', :as => 'company_registration'
+  end
 end
